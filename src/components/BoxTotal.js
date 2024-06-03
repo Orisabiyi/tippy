167c38ad5966adcd5acd829f59f70e3cc8ba8bbe
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-03 16:02:42 +0100

    created a button in BoxTotal component and styled box amount, button in the index.css file

--- src/components/BoxTotal.js
+++ src/components/BoxTotal.js
@@ -1,3 +1,24 @@
 export default function BoxTotal() {
-  return <div className="box-total"></div>;
+  return (
+    <div className="box">
+      <div className="box-amount">
+        <p>
+          <span>Tip Amount</span>
+          <span>/ person</span>
+        </p>
+
+        <h1>$0.00</h1>
+      </div>
+
+      <div className="box-amount">
+        <p>
+          <span>Tip Amount</span>
+          <span>/ person</span>
+        </p>
+        <h1>$0.00</h1>
+      </div>
+
+      <button>reset</button>
+    </div>
+  );
 }
