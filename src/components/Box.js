48aa77f633729acd13d0e7ff4af65d910bf111cf
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-03 14:00:33 +0100

    included the jsx for the first box and created a css file

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,8 +2,22 @@ export default function Box() {
   return (
     <div className="box">
       <div className="box-bill">
-        <label>Bill</label>
-        <input type="number" />
+        <label for="bill">Bill</label>
+        <input type="number" id="bill" />
+      </div>
+
+      <div className="box-tip">
+        <label>Select tip %</label>
+        <button>5%</button>
+        <button>10%</button>
+        <button>15%</button>
+        <button>25%</button>
+        <button>50%</button>
+      </div>
+
+      <div className="box-people">
+        <label for="people">Number of People</label>
+        <input type="number" id="people" />
       </div>
     </div>
   );
