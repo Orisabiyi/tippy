a22227e49dec7c87ed5c6cef1b7b992903675bae
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-03 15:31:11 +0100

    created a boxtotal component nd include it in the main component

---
+++ src/components/BoxTotal.js
@@ -0,0 +1,3 @@
+export default function BoxTotal() {
+  return <div className="box-total"></div>;
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import BoxBill from "./BoxBill";
+import BoxTotal from "./BoxTotal";
 
 export default function Main() {
   return (
     <div className="main">
       <BoxBill />
+      <BoxTotal />
     </div>
   );
 }
